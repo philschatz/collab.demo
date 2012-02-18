8c7a693c84aef9fafb293213cdf5c64462275de3
unknown <David@.(none)>
2012-02-18 13:19:06 -0600

    Add solution button to exercise plugin

--- plugins/exercise/editor_plugin_src.js
+++ plugins/exercise/editor_plugin_src.js
@@ -5,21 +5,35 @@
 	tinymce.create('tinymce.plugins.ExercisePlugin', {
 		init : function(ed, url) {
 			ed.addCommand('mceExercise', function() {
-                var el = ed.dom.create('exercise', {},
-                                       '<problem><p>[Enter your problem here]</p></problem><solution><p>[Enter your solution here]</p></solution><p>&nbsp;</p>');
-                ed.selection.setNode(el);
-                console.debug(tinyMCE.activeEditor.getContent());
+					var el = ed.dom.create('exercise', {},
+						'<problem><p>[Enter your problem here]</p></problem><solution><p>[Enter your solution here]</p></solution><p>&nbsp;</p>');
+                                        ed.selection.setNode(el);
+                                        console.debug(tinyMCE.activeEditor.getContent());
 			});
-
+			ed.addCommand('mceSolution', function() {
+					var selected = ed.selection.getNode();
+					var par = ed.dom.getParent(selected,'problem,solution');
+					ed.selection.select(par);
+					ed.selection.collapse(false);
+					var el = ed.dom.create('solution',{},'<p>[Enter your solution here]</p>');
+					ed.selection.setNode(el);
+			});
+			
 			ed.addButton('exercise', {
 				title : 'exercise.desc',
 				cmd : 'mceExercise',
 				image : url + '/img/exercise.gif'
 			});
-
+			ed.addButton('solution', {
+				title : 'solution.desc',
+				cmd : 'mceSolution',
+				image : url + '/img/solution.gif'
+			});
+		
 			ed.onNodeChange.add(function(ed, cm, n) {
 				var exercise = ed.dom.getParent(n, 'exercise');
-                cm.setDisabled('exercise', exercise != null);
+				cm.setDisabled('exercise', exercise != null);
+				cm.setDisabled('solution', ed.dom.getParent(n, 'problem,solution') == null);
 			});
 		},
 
---
+++ plugins/section/editor_plugin_src.js
@@ -0,0 +1,64 @@
+(function() {
+	// Load plugin specific language pack
+	// tinymce.PluginManager.requireLangPack('section');
+
+	tinymce.create('tinymce.plugins.SectionPlugin', {
+			init : function(ed, ul) {
+				ed.addCommand('mceSection', function(ui,v) {
+					    if (v==true) {
+						 var el = ed.dom.create('section', {}, '<p>[Enter section content here]</p>');
+						 ed.selection.setNode(el);   
+					    } else {
+						 var selected = ed.selection.getNode();
+						 var par = ed.dom.getParent(selected,'section');
+						 ed.selection.select(par);
+						 ed.selection.collapse(false);
+						 var el = ed.dom.create('section', {}, '<p>[Enter section content here]</p>');
+						 ed.selection.setNode(el); 
+					    }
+                                });
+                        },
+	    createControl: function(n, cm) {
+			   switch (n) {
+			    case 'sectionsplitbutton':
+				var c = cm.createSplitButton('sectionsplitbutton', {
+				    title : 'Section',
+				    image : 'img/exercise.gif',
+				    onclick : function() {
+					tinyMCE.activeEditor.execCommand('mceSection',false,false);
+				    }
+				});
+		
+				c.onRenderMenu.add(function(c, m) {
+				    m.add({title : 'Section', onclick : function() {
+					tinyMCE.activeEditor.execCommand('mceSection',false,false);
+				    }});
+		
+				    m.add({title : 'Subsection', onclick : function() {
+					tinyMCE.activeEditor.execCommand('mceSection',false,true);
+				    }});
+				});
+		
+				// Return the new splitbutton instance
+				return c;
+			}
+		
+			return null;
+		    },
+	    
+
+		getInfo : function() {
+			return {
+				longname : 'Section Plugin',
+				author : 'Roché Compaan',
+				authorurl : '',
+				infourl : '',
+				version : "1.0"
+			};
+		}
+	});
+
+
+	// Register plugin
+	tinymce.PluginManager.add('section', tinymce.plugins.SectionPlugin);
+})();
