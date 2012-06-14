743dd4f59dd1f9f91030bec5adf2001cd8d66429
Paul Brian <paul@mikadosoftware.com>
2012-06-14 13:38:57 +0100

    split out tiny initialisation into own js file

---
+++ tiny-init.js
@@ -0,0 +1,19 @@
+//JS file providing all init functions for tinymce editor ala oerpub
+
+tinyMCE.init({
+        mode: "textareas",
+        theme: "advanced",
+        theme_advanced_buttons1: "bold,italic,|,undo,redo,|,bullist,numlist,|,fullscreen",
+        theme_advanced_buttons2: "exercise,note,figure,section,|,title,label,|,solution,sectionsplitbutton",
+        theme_advanced_buttons3: "",
+        extended_valid_elements: "figure,caption",
+        custom_elements: "figure,caption",
+        valid_children: "+body[figure],+p[figure],figure[caption]",
+        doctype: "<!DOCTYPE html>",
+        content_css: "custom_content.css",
+        plugins: "blockish,section,fullscreen",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_statusbar_location : "bottom",
+
+});
+
